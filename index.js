c7c95e0616b3602f4a9765109b32dabe463b4da2
Taylor Tripp <tet8937@gmail.com>
2026-08-05 02:17:16 -0400

    Include team in game_started players list

--- index.js
+++ index.js
@@ -195,7 +195,7 @@ io.on('connection', socket => {
     room.started = true; room.state = initialState; room.difficulty = difficulty || 'medium';
     const hostPlayer = room.players.find(p => p.id === socket.id);
     cb?.({ ok: true, seat: hostPlayer?.seat ?? 0 });
-    io.to(room.code).emit('game_started', { state: initialState, players: room.players.map(p => ({ username: p.username, seat: p.seat })), difficulty: room.difficulty });
+    io.to(room.code).emit('game_started', { state: initialState, players: room.players.map(p => ({ username: p.username, seat: p.seat, team: p.team ?? p.seat % 2 })), difficulty: room.difficulty });
   });
 
   socket.on('action', ({ type, payload }) => {
